7f0ec4a28d291d67a315b974330fb35f938f3abc
Thijn <Ralkey@outlook.com>
2023-05-16 13:55:23 +0200

    goofy

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+	<NuxtLayout name="layout">
+		test
+	</NuxtLayout>
+	<!-- 123// -->
   </div>
 </template>
---
+++ layouts/layout.vue
@@ -0,0 +1,22 @@
+<style src="./layout.module.css" scoped />
+
+<script>
+export default {
+  data() {
+    return {
+      bool: true,
+      name: "test",
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    :class="{
+      name: bool,
+    }"
+  >
+    <slot />
+  </div>
+</template>
