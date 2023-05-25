ba87ca2e99b528007a7cd3f99c474be0669c0188
MrPotato-04 <80412415+MrPotato-04@users.noreply.github.com>
2023-05-25 12:50:48 +0200

    fix alle pages naar home page

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
-<script>
-import index from "./pages/index.vue";
-
-export default {
-  components: {
-    index,
-  },
-};
-</script>
-
 <template>
   <NuxtLayout name="layout">
-    <index />
+    <NuxtPage />
   </NuxtLayout>
 </template>
--- pages/producten/index.vue
+++ pages/producten/index.vue
@@ -1,20 +1,15 @@
 <script>
-
 export default {
- data() {
-  return {
-    items: [
-      {name: 'Product 1', price: 100, quantity: 1}
-    ]
-
-   }
-  }
- }
-
+  data() {
+    return {
+      items: [{ name: "Product 1", price: 100, quantity: 1 }],
+    };
+  },
+};
 </script>
 
 <template>
-    <div>
-        <b-table class="producten_table"></b-table>
-    </div>
+  <div>
+    <b-table class="producten_table"></b-table>
+  </div>
 </template>
