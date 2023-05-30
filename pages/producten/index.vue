1b9729a14f2d9d0fd236728c886001ed81ba5894
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2023-05-30 12:00:16 +0200

    product overzicht aangemaakt

--- pages/producten/index.vue
+++ pages/producten/index.vue
@@ -1,15 +1,35 @@
+<style src="./producten.css" scoped/>
+<template>
+  <div class="product_table">
+    <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>price</th>
+        <th>quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(product) in items">
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.quantity }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      items: [{ name: "Product 1", price: 100, quantity: 1 }],
-    };
-  },
-};
+      items: [
+        { name: "Product 1", price: "$0.21", quantity: 1 },
+        { name: "Product 2", price: "$20.00", quantity: 200 }
+      ]
+    }
+  }
+}
 </script>
-
-<template>
-  <div>
-    <b-table class="producten_table"></b-table>
-  </div>
-</template>
