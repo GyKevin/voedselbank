dafa8d99ead86c0cee5ff1509c5073805c11083d
Thijn <Ralkey@outlook.com>
2023-05-31 14:53:56 +0200

    added more

--- pages/admin/overzicht/gebruikers/index.vue
+++ pages/admin/overzicht/gebruikers/index.vue
@@ -10,21 +10,25 @@ export default {
           id: 1,
           name: "Harold",
           email: "HaroldW@gmail.com",
+          functie: 0,
         },
         {
           id: 2,
           name: "Frank",
           email: "fortKnox@outlook.com",
+          functie: 2,
         },
         {
           id: 3,
           name: "Wiljan",
           email: "wiljanverhoeven@gmail.com",
+          functie: 3,
         },
         {
           id: 4,
           name: "Kevin",
           email: "hiiiren@gmail.com",
+          functie: 2,
         },
       ],
     };
@@ -42,13 +46,20 @@ export default {
           <th>Id</th>
           <th>Naam</th>
           <th>Email</th>
+          <th>Functie</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="user in users">
           <td class="min">{{ user.id }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
+          <td>{{ user.name ?? "-" }}</td>
+          <td>{{ user.email ?? "-" }}</td>
+          <td>
+            <span v-if="user.functie === 0">Directie</span>
+            <span v-else-if="user.functie === 1">Magazijnmedewerker</span>
+            <span v-else-if="user.functie === 2">Vrijwilliger</span>
+            <span v-else>-</span>
+          </td>
         </tr>
       </tbody>
     </table>
