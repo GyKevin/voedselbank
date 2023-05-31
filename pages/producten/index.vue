d36fb0262b44285520a4e9063b7ac5c1349225ff
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2023-05-31 10:36:59 +0200

    made changes based on comments

--- pages/producten/index.vue
+++ pages/producten/index.vue
@@ -1,23 +1,26 @@
 <style src="./producten.css" scoped/>
-<template>
-  <div class="product_table">
-    <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>price</th>
-        <th>quantity</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(product) in items">
-        <td>{{ product.name }}</td>
-        <td>{{ product.price }}</td>
-        <td>{{ product.quantity }}</td>
-      </tr>
-    </tbody>
-  </table>
 
+<template>
+  <div class="pwindow">
+    <h3>Producten overzicht</h3>
+      <div class="product_table">
+        <table>
+          <thead>
+            <tr>
+              <th>Naam</th>
+              <th>Prijs</th>
+              <th>Aantal</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(product) in items">
+              <td>{{ product.name }}</td>
+              <td>{{ product.price }}</td>
+              <td>{{ product.quantity }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
   </div>
 </template>
 
