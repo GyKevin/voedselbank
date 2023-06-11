178ff3f81e248b913f0d50aeee624a820ab9955d
Thijn <Ralkey@outlook.com>
2023-06-11 10:23:31 +0200

    finished

--- pages/admin/overzicht/GemaakteVoedselPakketten/[pakketId]/index.vue
+++ pages/admin/overzicht/GemaakteVoedselPakketten/[pakketId]/index.vue
@@ -1,30 +1,21 @@
 <style src="./index.css" scoped />
 
 <script>
-import { Form, Field, ErrorMessage } from "vee-validate";
-import * as yup from "yup";
-
 export default {
   data() {
     return {
-      naamRules: yup.string().required(),
-      telefoonRules: yup.number().required(),
-      adresRules: yup.string().required(),
-      postcodeRules: yup
-        .string()
-        .required()
-        .matches(/^[\d]{4}( )?[A-Z]{2}$/, "Dit is geen geldige postcode"),
-      emailRules: yup.string().email().required(),
-      volwassenenRules: yup.number().required().integer().min(0),
-      jongerenRules: yup.number().required().integer().min(0),
-      babiesRules: yup.number().required().integer().min(0),
+      dateFormatOptions: {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      },
     };
   },
   setup() {
     const route = useRoute();
 
     const {
-      data: gezin,
+      data: voedselpakket,
       pending,
       error,
       refresh,
@@ -35,89 +26,63 @@ export default {
       },
     });
 
-    return { gezin, pending, error, refresh };
-  },
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
-  methods: {
-    onSubmit(values) {
-      const _values = { ...values, id: this.gezin.id, postcode: values.postcode.replace(/\s/g, "") };
-
-      useFetch(`/api/overzicht/gezinnen/${this.gezin.id}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(_values),
-      }).then((data) => {
-        this.refresh();
-        navigateTo("/admin/overzicht/gezinnen", { replace: true });
-      });
-    },
+    return { voedselpakket, pending, error, refresh };
   },
 };
 </script>
 
 <template>
-  <div v-if="!!gezin">
-    <h4>{{ gezin.naam }}'s gezin</h4>
+  <div v-if="!!voedselpakket" class="container">
+    <h4>voedselpakket {{ voedselpakket.voedselpakket_id }}</h4>
+    <h5>{{ voedselpakket.klanten_naam }}</h5>
 
-    <Form @submit="onSubmit">
-      <div class="formContent">
-        <div>
-          <label for="naam">Naam</label>
-          <Field type="text" name="naam" :value="gezin.naam" :rules="naamRules" />
-          <ErrorMessage name="naam" />
-        </div>
+    <table class="dateInfo">
+      <tbody>
+        <tr>
+          <td>Datum aanmaak</td>
+          <td>
+            {{
+              voedselpakket.datum_aanmaaken
+                ? new Date(voedselpakket.datum_aanmaaken).toLocaleDateString("en-NL", dateFormatOptions)
+                : "-"
+            }}
+          </td>
+        </tr>
+        <tr>
+          <td>Datum uitgifte</td>
+          <td>
+            {{
+              voedselpakket.datum_uitgifte
+                ? new Date(voedselpakket.datum_uitgifte).toLocaleDateString("en-NL", dateFormatOptions)
+                : "-"
+            }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-        <div>
-          <label for="telefoon">Telefoon</label>
-          <Field type="tel" name="telefoon" :value="gezin.telefoon" :rules="telefoonRules" />
-          <ErrorMessage name="telefoon" />
-        </div>
-
-        <div>
-          <label for="adres">Adres</label>
-          <Field type="text" name="adres" :value="gezin.adres" :rules="adresRules" />
-          <ErrorMessage name="adres" />
-        </div>
-
-        <div>
-          <label for="postcode">Postcode</label>
-          <Field type="text" name="postcode" :value="gezin.postcode" :rules="postcodeRules" />
-          <ErrorMessage name="postcode" />
-        </div>
-
-        <div>
-          <label for="email">Email</label>
-          <Field type="email" name="email" :value="gezin.email" :rules="emailRules" />
-          <ErrorMessage name="email" />
-        </div>
-
-        <div>
-          <label for="volwassenen">Volwassenen</label>
-          <Field type="number" name="volwassenen" :value="gezin.volwassenen" :rules="volwassenenRules" />
-          <ErrorMessage name="volwassenen" />
-        </div>
-
-        <div>
-          <label for="jongeren">Jongeren</label>
-          <Field type="number" name="jongeren" :value="gezin.jongeren" :rules="jongerenRules" />
-          <ErrorMessage name="jongeren" />
-        </div>
-
-        <div>
-          <label for="babies">Babies</label>
-          <Field type="number" name="babies" :value="gezin.baby" :rules="babiesRules" />
-          <ErrorMessage name="babies" />
-        </div>
-      </div>
-
-      <Button>Accept</Button>
-    </Form>
+    <div class="tableWrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>Product naam</th>
+            <th>Aantal</th>
+            <th>ean</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in voedselpakket.producten">
+            <td>{{ product.producten_naam ?? "-" }}</td>
+            <td>{{ product.product_aantal ?? "-" }}</td>
+            <td>{{ product.producten_ean ?? "-" }}</td>
+          </tr>
+          <tr v-if="!voedselpakket.producten[0]">
+            <td class="min">Geen producten beschikbaar.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- end of table wrapper -->
   </div>
 
   <!-- error -->
