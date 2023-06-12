0632af42bf5629b8a41e0ab1d501285f09758b43
Kevin <kvindso@gmail.com>
2023-06-12 11:59:14 +0200

    query bug

--- server/api/overzicht/gebruikers/index.get.ts
+++ server/api/overzicht/gebruikers/index.get.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
   const con = getMysqlConnection();
 
   try {
-    const [results, fields] = await con.promise().query("SELECT id, naam, email, functie FROM gebruikers");
+    const [results, fields] = await con.promise().execute("SELECT id, naam, email, functie FROM gebruikers");
 
     return results;
   } catch (error) {
