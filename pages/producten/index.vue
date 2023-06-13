80341d5a36b43d1bab858b8056463f2debbb3c45
Kevin <kvindso@gmail.com>
2023-06-13 10:30:07 +0200

    producten overzicht rework
    searchbar werkt nog niet, de design past nu bij de website en de data is nu van de database opgehaald

--- pages/producten/index.vue
+++ pages/producten/index.vue
@@ -1,38 +1,53 @@
-<style src="./producten.css" scoped/>
-
 <template>
-  <div class="pwindow">
-    <h3>Producten overzicht</h3>
-      <div class="product_table">
-        <table>
-          <thead>
-            <tr>
-              <th>Naam</th>
-              <th>Prijs</th>
-              <th>Aantal</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(product) in items">
-              <td>{{ product.name }}</td>
-              <td>{{ product.price }}</td>
-              <td>{{ product.quantity }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+  <h4>Producten overzicht</h4>
+
+  <!-- <p v-if="error">{{ error }}</p> -->
+  <div >
+    <input type="text" v-model="searchTerm" placeholder="Search users" />
+    <table>
+      <thead>
+        <tr>
+          <th>Ean</th>
+          <th>Naam</th>
+          <th>Categorie ID</th>
+          <th>Aantal</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in producten">
+          <td>{{ product.ean }}</td>
+          <td>{{ product.naam }}</td>
+          <td>{{ product.categorie_id }}</td>
+          <td>{{ product.aantal }}</td>
+        </tr>
+        <tr v-if="!producten[0]">
+          <td class="min">Geen producten beschikbaar.</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script setup>
+const {
+  data: producten,
+  pending,
+  error,
+} = useFetch("/api/overzicht/producten", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+</script>
 
 <script>
 export default {
   data() {
     return {
-      items: [
-        { name: "Product 1", price: "$0.21", quantity: 1 },
-        { name: "Product 2", price: "$20.00", quantity: 200 }
-      ]
+      searchTerm: "",
     }
   }
 }
 </script>
+<style src="./producten.css" scoped />
