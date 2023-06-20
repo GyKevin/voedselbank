ab9f6f2c3de2ab6cc8893cb7bf9f02b314d82199
Thijn <Ralkey@outlook.com>
2023-06-20 12:24:54 +0200

    Revert "Voed 23"

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -2,6 +2,8 @@
 <script setup>
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+const { isDesktop } = useDevice();
 </script>
 
 <script>
@@ -16,24 +18,12 @@ export default {
   data() {
     return {
       menuOpen: this.$device.isDesktop,
-      loggedIn: false,
     };
-  },
-  mounted() {
-    const cookieStatus = document.cookie.includes("user_id");
-    if (cookieStatus) {
-      this.loggedIn = true;
-    }
   },
   // actions
   methods: {
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
-    },
-    logout() {
-      document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      this.loggedIn = false;
     },
   },
 };
@@ -58,27 +48,20 @@ export default {
       </button>
 
       <div class="sidebarContainer">
-        <div class="logoContainer" @click="() => navigateTo('/', { replace: true })">
+        <div class="logoContainer">
           <img class="logo" src="~/assets/Logo.png" />
         </div>
 
         <div class="sidebarMenu">
           <div class="menu">
-            <NuxtLink to="/">Home</NuxtLink>
-            <NuxtLink to="/producten">Producten overzicht</NuxtLink>
-            <NuxtLink to="/admin/overzicht/gebruikers">Gebruikers overzicht</NuxtLink>
-            <NuxtLink to="/admin/overzicht/gezinnen">Gezinnen overzicht</NuxtLink>
-            <NuxtLink to="/admin/leveranciers">Leverancieren overzicht</NuxtLink>
-            <NuxtLink to="/admin/overzicht/VoedselPakketten">Voedsel pakketten overzicht</NuxtLink>
+            <a href="/" class="menuItem">Home</a>
+            <a href="/producten" class="menuItem">Producten overzicht</a>
+            <a href="/producten" class="menuItem">Item 3</a>
+            <a href="/producten" class="menuItem">Item 4</a>
           </div>
 
           <div class="bottomMenu">
-            <NuxtLink to="/login" class="login" v-if="loggedIn === false">
-              <font-awesome-icon :icon="['fass', 'circle-user']" /> Inloggen
-            </NuxtLink>
-            <NuxtLink to="/login" class="login" v-if="loggedIn === true" @click="logout">
-              <font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" /> Uitloggen
-            </NuxtLink>
+            <a href="#" class="login"> <font-awesome-icon :icon="['fass', 'circle-user']" /> Login </a>
           </div>
         </div>
       </div>
